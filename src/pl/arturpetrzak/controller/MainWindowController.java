699d6f8ba4c310abddb33b03df3ab95fe7996864
Artur Petrzak <petrzmax@gmail.com>
2021-04-26 16:12:50 +0200

    Update MainWindow Controller

--- src/pl/arturpetrzak/controller/MainWindowController.java
+++ src/pl/arturpetrzak/controller/MainWindowController.java
@@ -1,8 +1,19 @@
 package pl.arturpetrzak.controller;
 
 import pl.arturpetrzak.view.ViewFactory;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class MainWindowController extends BaseController{
+
+
+    @FXML
+    private TextField countryTextField;
+
+    @FXML
+    private TextField cityTextField;
+
+
     public MainWindowController(ViewFactory viewFactory, String fxmlName) {
         super(viewFactory, fxmlName);
     }
