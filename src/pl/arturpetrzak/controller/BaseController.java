1ee8adcabb8d8e12f1ec483a019f7f4e9e2879c5
Artur Petrzak <petrzmax@gmail.com>
2021-04-26 13:12:11 +0200

    Create base controller & view factory

---
+++ src/pl/arturpetrzak/controller/BaseController.java
@@ -0,0 +1,17 @@
+package pl.arturpetrzak.controller;
+
+import pl.arturpetrzak.view.ViewFactory;
+
+public class BaseController {
+    protected ViewFactory viewFactory;
+    private String fxmlName;
+
+    public BaseController(ViewFactory viewFactory, String fxmlName) {
+        this.viewFactory = viewFactory;
+        this.fxmlName = fxmlName;
+    }
+
+    public String getFxmlName() {
+        return fxmlName;
+    }
+}
