da3162d7a26760fc0e2b9e20490d98d64c764be0
Artur Petrzak <petrzmax@gmail.com>
2021-04-26 13:12:29 +0200

    Create Main window controller

---
+++ src/pl/arturpetrzak/controller/MainWindowController.java
@@ -0,0 +1,9 @@
+package pl.arturpetrzak.controller;
+
+import pl.arturpetrzak.view.ViewFactory;
+
+public class MainWindowController extends BaseController{
+    public MainWindowController(ViewFactory viewFactory, String fxmlName) {
+        super(viewFactory, fxmlName);
+    }
+}
