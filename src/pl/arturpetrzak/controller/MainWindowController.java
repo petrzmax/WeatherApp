cb4b03ad87c169745b4953204dfda717c1ee9ac0
Artur Petrzak <petrzmax@gmail.com>
2021-05-17 18:42:31 +0200

    Develop MainWindow view

--- src/pl/arturpetrzak/controller/MainWindowController.java
+++ src/pl/arturpetrzak/controller/MainWindowController.java
@@ -1,10 +1,17 @@
 package pl.arturpetrzak.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import pl.arturpetrzak.view.ViewFactory;
 
 public class MainWindowController extends BaseController{
 
+    @FXML
+    private Label currentCountryLabel;
+
+    @FXML
+    private Label currentCityLabel;
 
     @FXML
     private TextField countryTextField;
@@ -13,6 +20,8 @@ public class MainWindowController extends BaseController{
     private TextField cityTextField;
 
     @FXML
+    void refreshAction() {
+        
     }
 
 
