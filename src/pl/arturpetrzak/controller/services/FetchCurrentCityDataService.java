b4ee3172320f9e50c18e2258942db849ba1df043
Artur Petrzak <petrzmax@gmail.com>
2021-05-18 15:45:37 +0200

    Update class name

--- src/pl/arturpetrzak/controller/services/FetchCurrentCityDataService.java
+++ src/pl/arturpetrzak/controller/services/FetchCurrentCityDataService.java
@@ -3,10 +3,10 @@ package pl.arturpetrzak.controller.services;
 import org.json.JSONObject;
 import pl.arturpetrzak.Config;
 
-public class FetchCurrentCityNameService extends BaseApiService{
+public class FetchCurrentCityDataService extends BaseApiService{
     protected final String API_KEY_PREFIX = "access_key=";
 
-    public FetchCurrentCityNameService() {
+    public FetchCurrentCityDataService() {
         super();
         url = Config.getCurrentCityByIpApiUrl() + "?" + API_KEY_PREFIX + Config.getIpstackApiKey();
     }
