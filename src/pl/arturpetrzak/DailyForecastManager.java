3e94044442db99e36e1e66c4bee18eb447f04d2b
Artur Petrzak <petrzmax@gmail.com>
2021-05-19 14:23:58 +0200

    Add fetching current City weather prototype

--- src/pl/arturpetrzak/DailyForecastManager.java
+++ src/pl/arturpetrzak/DailyForecastManager.java
@@ -2,6 +2,9 @@ package pl.arturpetrzak;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import pl.arturpetrzak.controller.services.FetchCityDataService;
+import pl.arturpetrzak.controller.services.FetchCurrentLocalizationService;
+import pl.arturpetrzak.controller.services.FetchWeatherService;
 import pl.arturpetrzak.model.DailyForecast;
 
 import java.util.ArrayList;
@@ -10,6 +13,9 @@ import java.util.List;
 public class DailyForecastManager {
     private JSONObject headline;
     private List<DailyForecast> dailyForecasts = new ArrayList();
+    private String currentCountryName = "";
+    private String currentCityName = "";
+    private String currentCityId = "";
 
     public void loadWeatherData (JSONObject weatherData) {
         headline = weatherData.getJSONObject("Headline");
@@ -19,5 +25,41 @@ public class DailyForecastManager {
             DailyForecast dailyForecast = new DailyForecast((JSONObject) object);
             dailyForecasts.add(dailyForecast);
         }
+    }
+
+    public void getCurrentCityData() {
+        FetchCurrentLocalizationService fetchCurrentLocalizationService = new FetchCurrentLocalizationService();
+        fetchCurrentLocalizationService.start();
+        fetchCurrentLocalizationService.setOnSucceeded(event -> {
+            currentCountryName = fetchCurrentLocalizationService.getCountryName();
+            currentCityName = fetchCurrentLocalizationService.getCityName();
+            getCurrentCityId();
+        });
+    }
+
+    public void getCurrentCityId() {
+        FetchCityDataService fetchCityDataService = new FetchCityDataService(currentCountryName, currentCityName);
+        fetchCityDataService.start();
+        fetchCityDataService.setOnSucceeded(event -> {
+            currentCityId = fetchCityDataService.getCityId();
+            getCurrentCityWeatherData();
+        });
+    }
+
+    private void getCurrentCityWeatherData() {
+        FetchWeatherService fetchWeatherService = new FetchWeatherService(currentCityId);
+        fetchWeatherService.start();
+        fetchWeatherService.setOnSucceeded(event -> {
+            loadWeatherData(fetchWeatherService.getWeatherData());
+
+        });
+    }
+
+    public String getCurrentCountryName() {
+        return currentCountryName;
+    }
+
+    public String getCurrentCityName() {
+        return currentCityName;
     }
 }
