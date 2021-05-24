b908cf72c59bb36cc7c646e82f15d2bd24cb2d25
Artur Petrzak <petrzmax@gmail.com>
2021-05-24 12:30:25 +0200

    Improve forecast representation

--- src/pl/arturpetrzak/view/DailyForecastRepresentation.java
+++ src/pl/arturpetrzak/view/DailyForecastRepresentation.java
@@ -4,34 +4,50 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 import pl.arturpetrzak.model.DailyForecast;
 
 public class DailyForecastRepresentation {
     public VBox getDailyForecastRepresentation(DailyForecast dailyForecast, ImageView dayImageView, ImageView nightImageView) {
 
+        Label label;
         VBox vBox = new VBox();
         vBox.setAlignment(Pos.CENTER);
         vBox.setMinWidth(130);
-        vBox.setMaxHeight(200);
 
+        // Date
+        label = new Label(dailyForecast.getDate());
+        label.setTextAlignment(TextAlignment.CENTER);
+        label.setFont(Font.font(20));
+        vBox.getChildren().add(label);
+
+        // Day
         vBox.getChildren().add(dayImageView);
+        label = new Label(dailyForecast.getDayWeatherDescription().replace("w/", "with"));
 
-        Label label = new Label(dailyForecast.getDayWeatherDescription());
+
         label.setWrapText(true);
         label.setTextAlignment(TextAlignment.CENTER);
         label.setMinHeight(40);
-
         vBox.getChildren().add(label);
-        vBox.getChildren().add(new Label(dailyForecast.getDate()));
-        vBox.getChildren().add(new Label("Temperatures:"));
-        vBox.getChildren().add(new Label("Max: " + dailyForecast.getMaximumTemperature() + dailyForecast.getUnit()));
-        vBox.getChildren().add(new Label("Min: " + dailyForecast.getMinimumTemperature() + dailyForecast.getUnit()));
 
+        // Night
         vBox.getChildren().add(nightImageView);
-        label = new Label(dailyForecast.getNightWeatherDescription());
+        label = new Label(dailyForecast.getNightWeatherDescription().replace("w/", "with"));
         label.setWrapText(true);
         label.setMinHeight(40);
+        vBox.getChildren().add(label);
+
+        // Weather description
+
+        label = new Label("Temperatures:" +
+                "\nMax: " + dailyForecast.getMaximumTemperature() + dailyForecast.getUnit() +
+                "\nMin: " + dailyForecast.getMinimumTemperature() + dailyForecast.getUnit()
+        );
+
+        label.setTextAlignment(TextAlignment.CENTER);
+
         vBox.getChildren().add(label);
 
         return vBox;
