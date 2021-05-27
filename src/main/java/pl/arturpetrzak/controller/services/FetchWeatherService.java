7baed20167ba95114f087ea99db7b88660c69654
petrzmax <petrzmax@gmail.com>
2021-05-27 14:20:08 +0200

    Add option to make API response metric

--- src/main/java/pl/arturpetrzak/controller/services/FetchWeatherService.java
+++ src/main/java/pl/arturpetrzak/controller/services/FetchWeatherService.java
@@ -4,10 +4,11 @@ import org.json.JSONObject;
 import pl.arturpetrzak.Config;
 
 public class FetchWeatherService extends BaseApiService {
+    private final String METRIC_PREFIX = "metric=";
 
-    public FetchWeatherService(String cityId) {
+    public FetchWeatherService(String cityId, boolean metric) {
         super();
-        url = Config.getDailyWeatherForecastApiUrl() + cityId + "?" + API_KEY_PREFIX + Config.getAccuWeatherApiKey();
+        url = Config.getDailyWeatherForecastApiUrl() + cityId + "?" + METRIC_PREFIX + metric + "&" + API_KEY_PREFIX + Config.getAccuWeatherApiKey();
     }
 
     public JSONObject getWeatherData() {
