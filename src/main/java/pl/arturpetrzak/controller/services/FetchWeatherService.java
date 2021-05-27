751721c80bcecd6bbfd8923284b3fce20efff9a7
petrzmax <petrzmax@gmail.com>
2021-05-27 14:36:44 +0200

    Make language choosable

--- src/main/java/pl/arturpetrzak/controller/services/FetchWeatherService.java
+++ src/main/java/pl/arturpetrzak/controller/services/FetchWeatherService.java
@@ -5,10 +5,11 @@ import pl.arturpetrzak.Config;
 
 public class FetchWeatherService extends BaseApiService {
     private final String METRIC_PREFIX = "metric=";
+    private final String LANGUAGE_PREFIX = "language=";
 
-    public FetchWeatherService(String cityId, boolean metric) {
+    public FetchWeatherService(String cityId, boolean metric, String language) {
         super();
-        url = Config.getDailyWeatherForecastApiUrl() + cityId + "?" + METRIC_PREFIX + metric + "&" + API_KEY_PREFIX + Config.getAccuWeatherApiKey();
+        url = Config.getDailyWeatherForecastApiUrl() + cityId + "?" + METRIC_PREFIX + metric + "&" + LANGUAGE_PREFIX + language + "&" + API_KEY_PREFIX + Config.getAccuWeatherApiKey();
     }
 
     public JSONObject getWeatherData() {
