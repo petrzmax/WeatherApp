5bb6c140c20f49f11785dc8c44e4bdeaa52f4504
petrzmax <petrzmax@gmail.com>
2021-05-28 15:50:04 +0200

    Add validation messages

--- src/main/java/pl/arturpetrzak/Messages.java
+++ src/main/java/pl/arturpetrzak/Messages.java
@@ -9,10 +9,15 @@ public class Messages {
     public static final String FETCHING_CITY_ID = "Fetching city id";
     public static final String FETCHING_WEATHER_DATA= "Fetching weather data";
 
-    // Errors
+    // Services errors
     public static final String REQUEST_SYNTAX_ERROR = "Request syntax error occurred";
     public static final String API_AUTHORIZATION_ERROR = "API error occurred";
     public static final String SERVER_ERROR = "Server error occurred";
     public static final String UNEXPECTED_ERROR = "Unexpected error occurred";
     public static final String UNSUPPORTED_ERROR = "Unsupported fetching result";
+
+    // Validation errors
+    public static final String NO_CITY_NAME = "Provide city name to begin";
+    public static final String CITY_NAME_TOO_LONG = "City name is too long. It should be shorter than 50 characters";
+    public static final String COUNTRY_NAME_TOO_LONG = "Country name is too long. It should be shorter than 50 characters";
 }
