f2805ef5e6e3017ca7339f9095d026c89b332eca
petrzmax <petrzmax@gmail.com>
2021-05-31 13:29:29 +0200

    Implement initializable interface

--- src/main/java/pl/arturpetrzak/controller/SettingsWindowController.java
+++ src/main/java/pl/arturpetrzak/controller/SettingsWindowController.java
@@ -1,11 +1,15 @@
 package pl.arturpetrzak.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 import pl.arturpetrzak.view.ViewFactory;
 
-public class SettingsWindowController extends BaseController{
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SettingsWindowController extends BaseController implements Initializable {
 
 
     @FXML
@@ -26,5 +30,10 @@ public class SettingsWindowController extends BaseController{
 
     public SettingsWindowController(ViewFactory viewFactory, String fxmlName) {
         super(viewFactory, fxmlName);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
