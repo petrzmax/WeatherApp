fbf62ce79615c0887530030047b4b89937ed3a73
petrzmax <petrzmax@gmail.com>
2021-06-14 15:57:17 +0200

    Create temperature representation template & getter

--- src/main/java/pl/arturpetrzak/Messages.java
+++ src/main/java/pl/arturpetrzak/Messages.java
@@ -30,4 +30,14 @@ public class Messages {
 
     // Others
     public static final String DIALOG_TITLE = "Information Dialog";
+    private static String TEMPERATURE_REPRESENTATION_TEMPLATE = "Temperatures:\nMax: %2$s °%3$s\nMin: %1$s °%3$s";
+
+    public static String getTemperatureRepresentation (String minimumTemperature, String maximumTemperature, String unit) {
+        return String.format(
+                TEMPERATURE_REPRESENTATION_TEMPLATE,
+                minimumTemperature,
+                maximumTemperature,
+                unit
+        );
+    }
 }
