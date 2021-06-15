a697927f126103867605aff441061bafa5cb2802
petrzmax <petrzmax@gmail.com>
2021-06-15 16:12:49 +0200

    Remove unnecessary accessors

--- src/main/java/pl/arturpetrzak/model/LocationForecast.java
+++ src/main/java/pl/arturpetrzak/model/LocationForecast.java
@@ -36,27 +36,4 @@ public class LocationForecast {
         return dailyForecasts;
     }
 
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getCityId() {
-        return cityId;
-    }
-
-    public void setCityId(String cityId) {
-        this.cityId = cityId;
-    }
 }
