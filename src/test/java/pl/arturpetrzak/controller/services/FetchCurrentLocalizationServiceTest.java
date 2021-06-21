64a9f499910378a64f4002e893d4f1bdcb20ece0
petrzmax <petrzmax@gmail.com>
2021-06-21 18:13:01 +0200

    Update FetchCurrentLocalizationServiceTest.java

--- src/test/java/pl/arturpetrzak/controller/services/FetchCurrentLocalizationServiceTest.java
+++ src/test/java/pl/arturpetrzak/controller/services/FetchCurrentLocalizationServiceTest.java
@@ -3,26 +3,29 @@ package pl.arturpetrzak.controller.services;
 import com.adelean.inject.resources.junit.jupiter.GivenTextResource;
 import com.adelean.inject.resources.junit.jupiter.TestWithResources;
 import okhttp3.*;
-import org.hamcrest.core.AnyOf;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.arturpetrzak.Config;
 import pl.arturpetrzak.controller.FetchDataResult;
 
 import java.io.IOException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 @TestWithResources
 @ExtendWith(MockitoExtension.class)
@@ -52,8 +55,9 @@ class FetchCurrentLocalizationServiceTest {
 
     @BeforeAll
     static void setup() {
-       // MockedStatic<Config> config = Mockito.mockStatic(Config.class);
-       // config.when(Config::getIpstackApiKey).thenReturn("API_KEY");
+        MockedStatic<Config> config = Mockito.mockStatic(Config.class);
+        config.when(Config::getIpstackApiKey).thenReturn("TestApiKey");
+        config.when(Config::getCurrentCityByIpApiUrl).thenReturn("https://TestApiUrl");
     }
 
     @Test
@@ -96,8 +100,8 @@ class FetchCurrentLocalizationServiceTest {
         });
 
         assertThat(exception.getMessage(), is(equalTo(
-                    "HttpRequestCode: 101 - You have not supplied a valid API Access Key. [Technical Support: support@apilayer.com]"
-            )));
+                "HttpRequestCode: 101 - You have not supplied a valid API Access Key. [Technical Support: support@apilayer.com]"
+        )));
     }
 
     @Test
@@ -122,20 +126,16 @@ class FetchCurrentLocalizationServiceTest {
     }
 
     @Test
-    void shouldReturnResponseEmptyResult() throws Exception {
+    void shouldReturnSuccessFetchDataResult() throws Exception {
         //given
         given(okHttpClient.newCall(any(Request.class))).willReturn(call);
         given(response.code()).willReturn(200);
         given(call.execute()).willReturn(response);
         given(response.body()).willReturn(responseBody);
-        given(fetchCurrentLocalizationService.parseResponse(any(String.class))).willReturn(null);
+        given(responseBody.string()).willReturn(serverSuccessResponse);
 
-        //when
-        //then
-        assertThat(fetchCurrentLocalizationService.fetchData(), is(equalTo(FetchDataResult.RESPONSE_EMPTY)));
+        assertThat(fetchCurrentLocalizationService.fetchData(), is(equalTo(FetchDataResult.SUCCESS)));
     }
-
-
 
 
     @Test
@@ -171,7 +171,7 @@ class FetchCurrentLocalizationServiceTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3 , 222, 333, 444, 555})
+    @ValueSource(ints = {1, 2, 3, 222, 333, 444, 555})
     void shouldReturnFailedByUnexpectedError(int responseCode) {
 
         //when
